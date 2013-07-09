da36bf0275f96020d14a9623c0bdb21f17855af1
Paul Coleman <pcoleman@vision.org>
2013-07-09 14:52:09 -0700

    Initial Commit

---
+++ sites/all/themes/Visionmedia/templates/node--video-content.tpl.php
@@ -0,0 +1,40 @@
+<article<?php print $attributes; ?>>
+  <?php print $user_picture; ?>
+  
+  <?php if (!$page && $title): ?>
+  <header>
+    <?php print render($title_prefix); ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+    <?php print render($title_suffix); ?>
+  
+  </header>
+  <?php endif; ?>
+  
+  <?php if ($display_submitted): ?>
+  <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
+  <?php endif; ?>  
+  
+  <div<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content['field_sub_title']);   
+	//print render($Video_player);
+	
+	//print ($video_full_path);
+    ?>
+  </div>
+  <div style="padding-left:10%;padding-right:10%;padding-top:1em">
+<?php
+print render($content['body']);
+?>
+</div>
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+    <?php endif; ?>
+
+    <?php print render($content['comments']); ?>
+  </div>
+</article>
